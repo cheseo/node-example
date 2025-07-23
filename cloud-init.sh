39e4025503f247583a98bada580250004ee5b7e2
Mr. Me <me@example.tld>
2025-07-23 21:39:56 +0545

    use systemd to mangae app

--- cloud-init.sh
+++ cloud-init.sh
@@ -8,16 +8,36 @@ node.ec2.ashwink.com.np {
 }
 EOF
 
+user_service(){
+	cat
+}<<EOF
+[Unit]
+Description=My node app
+After=network.target
+
+[Service]
+WorkingDirectory=%h/node-example
+ExecStart=node app.js
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=default.target
+EOF
+
+user_service > /etc/systemd/user/node.service
+
 apt update
 apt install -y nodejs caddy
+
+config > /etc/caddy/Caddyfile
 
 sudo -Hu ubuntu bash <<EOF
 cd
 git clone "https://github.com/cheseo/node-example";
-printf '%s\n' '#!/bin/bash' 'cd ~/node-example' 'git pull' 'pkill node' 'node app.js' > update
-chmod +x update
+systemctl --user enable --now node.service
+loginctl enable-linger
 EOF
 
-config > /etc/caddy/Caddyfile
 
 reboot
