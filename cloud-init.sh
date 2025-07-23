e1ec120c797598da19d97c342ed0eeb3ce1ded83
me <me@example.com>
2025-07-23 19:26:55 +0545

    wrong names

--- cloud-init.sh
+++ cloud-init.sh
@@ -6,15 +6,14 @@ config(){
 node.ec2.ashwink.com.np {
 	reverse_proxy localhost:3000
 }
-
 EOF
 
 apt update
-apt install -y node caddy
+apt install -y nodejs caddy
 
 sudo -Hu ubuntu bash <<EOF
 cd
-git clone "https://github.com/cheseo/node-hello";
+git clone "https://github.com/cheseo/node-example";
 printf '%s\n' '#!/bin/bash' 'cd ~/node-hello' 'git pull' 'pkill node' 'node app.js' > update
 chmod +x update
 EOF
